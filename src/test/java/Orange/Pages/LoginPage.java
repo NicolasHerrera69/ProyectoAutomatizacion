52ec4da5d2b3382cde7e2c0210c01268f4d33e9f
Nicolas Herrera69 <nicolasrobayo44@gmail.com>
2021-09-27 18:05:40 -0500

    HU-001_CtreacionLogin

--- src/test/java/Orange/Pages/LoginPage.java
+++ src/test/java/Orange/Pages/LoginPage.java
@@ -2,40 +2,38 @@ package Orange.Pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.internal.MouseAction.Button;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
+import Orange.Steps.ButtonPages;
+
 public class LoginPage {
 	
-	private WebDriver driver;
-	
-	
+	private ButtonPages buttonPages;
+
 	@FindBy(how = How.ID, using = "txtUsername")
 	private WebElement inputUserName;
-	
-	
-	
-	
+
+	@FindBy(how = How.ID, using = "txtPassword")
+	private WebElement inputUserPassword;
+
 	public LoginPage(WebDriver driver) {
-		
+
 		PageFactory.initElements(driver, this);
-		
-		
+		this.buttonPages = new ButtonPages(driver);
+
 	}
-	
-	public void loginIn(String userName) {
-		
+
+	public void loginIn(String userName,String password) {
+
 		inputUserName.sendKeys(userName);
+		inputUserPassword.sendKeys(password);
+		buttonPages.btnLogin();
 		
 		
-		
-		
-		
-		
-		
-		
-		
+
 	}
 
 }
